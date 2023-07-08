5ba51ce89b2869dcae69ae528ac8e2d00d5be2d4
Jaspreet-Bhagat <138782706+Jaspreet-Bhagat@users.noreply.github.com>
2023-07-08 04:57:23 -0400

    Add files via upload

---
+++ DAY-1/Printing_to_console.py
@@ -0,0 +1,6 @@
+print("Welcome to the band Name Generator.\n")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("your band name could be",city + pet);
+
+
